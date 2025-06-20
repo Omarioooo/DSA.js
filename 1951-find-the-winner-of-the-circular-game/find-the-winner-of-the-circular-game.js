d487968e204b4f79a1c35e76935a66b498a0cc75
Omar Mohamed <omarrmohammed86@gmail.com>
2025-06-21 00:27:17 +0300

    Time: 0 ms (100.00%) | Memory: 54.9 MB (62.23%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.js
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.js
@@ -4,15 +4,10 @@
  * @return {number}
  */
 var findTheWinner = function(n, k) {
-    let arr = [];
-    for(let i = 1; i<=n; i++) arr.push(i);
-    
-    let curIndex = 0;
-    while(arr.length > 1){
-        curIndex = (curIndex + k - 1) % arr.length;
-
-        arr.splice(curIndex, 1)
+    function josephus(n) {
+        if (n === 1) return 0;
+        return (josephus(n - 1) + k) % n;
     }
 
-    return arr[0];
+    return josephus(n) + 1;
 };
